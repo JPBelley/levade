fc9e30d0f82959d4f614f53c4e0e6aa5d27bcf52
Jean-Philippe Belley <jean-philippe.belley@cossette.com>
2019-07-28 14:51:54 -0400

    hero and all gatbsy

---
+++ src/components/hero.js
@@ -0,0 +1,113 @@
+import React from "react";
+import Swiper from 'react-id-swiper';
+
+import 'react-id-swiper/lib/styles/scss/swiper.scss';
+import HeroImg from './hero/urbex2.jpg';
+import './hero.scss';
+
+
+const opacityStart = {
+  opacity: 1
+}
+const slide1 = {
+  backgroundImage: 'url(' + HeroImg + ')'
+};
+
+function getRandomArbitrary(min, max) {
+  return Math.random() * (max - min) + min;
+}
+
+var interleaveOffset = 0.5;
+
+const Hero = () => {
+
+  const params = {
+    // loop: true,
+    speed: 3000,
+    grabCursor: true,
+    watchSlidesProgress: true,
+    effect: 'fade',
+    mousewheelControl: true,
+    keyboardControl: true,
+    navigation: {
+      nextEl: ".swiper-button-next",
+      prevEl: ".swiper-button-prev"
+    },
+    on: {
+      progress: function () {
+        var swiper = this;
+        for (var i = 0; i < swiper.slides.length; i++) {
+          var slideProgress = swiper.slides[i].progress;
+          var innerOffset = swiper.width * interleaveOffset;
+          var innerTranslate = slideProgress * innerOffset;
+
+          // swiper.slides[i].querySelector(".headline").setAttribute(
+          //   "style", "transform: translate3d(" + innerTranslate + "px, 0, 0);");
+        }
+      },
+      touchStart: function () {
+        var swiper = this;
+        for (var i = 0; i < swiper.slides.length; i++) {
+          swiper.slides[i].style.transition = "";
+        }
+      },
+      setTransition: function (speed) {
+        var swiper = this;
+
+        for (var i = 0; i < swiper.slides.length; i++) {
+          swiper.slides[i].style.transition = speed + "ms";
+          // swiper.slides[i].querySelector(".headline").style.transition =
+          //   (speed * .85) + "ms";
+          swiper.slides[i].querySelectorAll(".single-square").forEach(function (square) {
+            square.style.transition =
+              getRandomArbitrary(0, 2) + "s";
+            square.style.opacity =
+              "1";
+          });
+        }
+      },
+      transitionStart: function (speed) {
+        var swiper = this;
+
+        for (var i = 0; i < swiper.slides.length; i++) {
+          if (!swiper.slides[i].classList.contains('swiper-slide-active')) {
+            swiper.slides[i].querySelectorAll(".single-square").forEach(function (square) {
+              square.style.opacity =
+                "0";
+            });
+          }
+        }
+      }
+    }
+  }
+
+  return (
+    <section className="swiper-container">
+      <Swiper {...params}>
+        <div className="swiper-slide hero">
+          <div className="square-container">
+            <div className="single-square" style={opacityStart}> <div style={slide1}></div> </div>
+            <div className="single-square" style={opacityStart}> <div style={slide1}></div> </div>
+            <div className="single-square" style={opacityStart}> <div style={slide1}></div> </div>
+            <div className="single-square" style={opacityStart}> <div style={slide1}></div> </div>
+            <div className="single-square" style={opacityStart}> <div style={slide1}></div> </div>
+            <div className="single-square" style={opacityStart}> <div style={slide1}></div> </div>
+            <div className="single-square" style={opacityStart}> <div style={slide1}></div> </div>
+            <div className="single-square" style={opacityStart}> <div style={slide1}></div> </div>
+            <div className="single-square" style={opacityStart}> <div style={slide1}></div> </div>
+            <div className="single-square" style={opacityStart}> <div style={slide1}></div> </div>
+            <div className="single-square" style={opacityStart}> <div style={slide1}></div> </div>
+            <div className="single-square" style={opacityStart}> <div style={slide1}></div> </div>
+          </div>
+          <div className="headline">
+            <h1>L'évadé</h1>
+          </div>
+        </div>
+      </Swiper>
+      {/* <div className="swiper-button-next"></div>
+      <div className="swiper-button-prev"></div> */}
+    </section>
+  )
+}
+
+export default Hero
--- src/components/layout.js
+++ src/components/layout.js
@@ -10,7 +10,7 @@ import PropTypes from "prop-types"
 import { useStaticQuery, graphql } from "gatsby"
 
 import Header from "./header"
-import "./layout.css"
+import "./layout.scss"
 
 const Layout = ({ children }) => {
   const data = useStaticQuery(graphql`
@@ -28,9 +28,6 @@ const Layout = ({ children }) => {
       <Header siteTitle={data.site.siteMetadata.title} />
       <div
         style={{
-          margin: `0 auto`,
-          maxWidth: 960,
-          padding: `0px 1.0875rem 1.45rem`,
           paddingTop: 0,
         }}
       >
