f5665e2e1a62621b4ee9878c4adfcb5331017bac
Jean-Philippe Belley <jean-philippe.belley@cossette.com>
2019-07-29 23:34:52 -0400

    Hero animation

--- src/components/singleCard/SingleCard.js
+++ src/components/singleCard/SingleCard.js
@@ -1,10 +1,16 @@
 import React from "react";
+import Place from '../../images/lieux/military_base.jpg';
 
+import './SingleCard.scss';
 
 const SingleCard = () => (
 
   <div className="card">
-    supppp
+    <div className="card-content">
+      <div className="card-text">La Base Militaire</div>
+      <div className="card-overlay"></div>
+      <img src={Place} alt="" />
+    </div>
   </div>
 )
 
